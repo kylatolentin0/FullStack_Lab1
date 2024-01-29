76f9256cec5f9638d501c8a1621797d56de5cde2
Kyla Tolentino <ktolentino2021@fau.edu>
2024-01-29 01:11:13 -0500

    Finished Lab 1

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Calendar from './components/Calendar';
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Kyla's Schedule</h1>
+      <h2>Let's Goooo!!!</h2>
+      <Calendar></Calendar>
+
+      
+    </div>
+  )
+}
+
+export default App
